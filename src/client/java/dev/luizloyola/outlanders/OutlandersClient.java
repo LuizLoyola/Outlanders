09a9eecff5c9bc9bc1d54828f3c2e734d6611512
Luiz Loyola <eu@luizloyola.com.br>
2026-01-31 04:34:58 -0300

    Initial Commit (From Fabric Template)

---
+++ src/client/java/dev/luizloyola/outlanders/OutlandersClient.java
@@ -0,0 +1,10 @@
+package dev.luizloyola.outlanders;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OutlandersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/dev/luizloyola/outlanders/OutlandersDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.luizloyola.outlanders;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OutlandersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
